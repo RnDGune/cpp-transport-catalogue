8ab6c55d48e197679612ffad9efe8131385847c9
RnDGune <97451676+RnDGune@users.noreply.github.com>
2022-06-02 20:57:48 +0700

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,19 @@
-// место для вашего кода
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+
+
+#include "input_reader.h"
+#include "transport_catalogue.h"
+#include <iostream>         
+#include <sstream>
+
+#include "geo.h"
+#include "stat_reader.h"
+
+int main()
+{
+    transport_catalogue::TransportCatalogue  transport_catalog;
+    transport_catalogue::input_reader::ProcessInput(transport_catalog);
+    transport_catalogue::stat_reader::ProcessRequests(transport_catalog);
+    return 0;
+}
